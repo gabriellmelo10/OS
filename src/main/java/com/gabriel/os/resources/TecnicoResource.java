99758421db71f9442e84c74dc1e81227fdcb3c58
gabriel <gabriellmelo@gmail.com>
2022-06-29 21:56:00 -0300

    Implementação de endpoint que lista todos os técnicos

--- src/main/java/com/gabriel/os/resources/TecnicoResource.java
+++ src/main/java/com/gabriel/os/resources/TecnicoResource.java
@@ -1,5 +1,9 @@
 package com.gabriel.os.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabriel.os.domain.Tecnico;
 import com.gabriel.os.dtos.TecnicoDTO;
 import com.gabriel.os.services.TecnicoService;
 
@@ -22,6 +27,26 @@ public class TecnicoResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 			TecnicoDTO objDTO = new TecnicoDTO(this.service.findById(id));
+			
 			return ResponseEntity.ok().body(objDTO);
-	} 
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<TecnicoDTO> listDTO = service.findAll().stream()
+				.map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+		
+//		List<Tecnico> list = this.service.findAll();
+//		List<TecnicoDTO> listDTO = new ArrayList<TecnicoDTO>();
+//		
+//		for(Tecnico tecnico : list) {
+//			listDTO.add(new TecnicoDTO(tecnico));
+//		}
+		
+//		
+		
+//		list.forEach(obj -> listDTO.add(new TecnicoDTO(obj)));
+	}
 }
--- src/main/java/com/gabriel/os/services/TecnicoService.java
+++ src/main/java/com/gabriel/os/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.gabriel.os.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class TecnicoService {
 		Optional<Tecnico> obj = this.repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + ", Tipo: " + Tecnico.class.getName()));
+	}
+
+	public List<Tecnico> findAll() {
+		return this.repository.findAll();
 	}
 }
