9575351923bbb24be389858451bdd59d6319b0d7
gabriel <gabriellmelo@gmail.com>
2022-07-01 15:45:07 -0300

    Implementação da validação dos campos vazios.

--- src/main/java/com/gabriel/os/resources/exceptions/ResourceExceptionHandle.java
+++ src/main/java/com/gabriel/os/resources/exceptions/ResourceExceptionHandle.java
@@ -2,6 +2,8 @@ package com.gabriel.os.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,4 +27,16 @@ public class ResourceExceptionHandle {
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(MethodArgumentNotValidException e) {
+		ValidationError error = new ValidationError(System.currentTimeMillis(), 
+				HttpStatus.BAD_REQUEST.value(), "Erro na validação dos campos!");
+		
+		for(FieldError objError : e.getBindingResult().getFieldErrors()) {
+			error.addErrors(objError.getField(), objError.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}	
 }
