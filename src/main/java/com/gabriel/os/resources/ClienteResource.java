6e37a1b1a1b2c8db173ac0a6ca98a684658b93b2
gabriel <gabriellmelo@gmail.com>
2022-07-20 22:37:28 -0300

    Implementação do CRUD do Cliente.

--- src/main/java/com/gabriel/os/resources/ClienteResource.java
+++ src/main/java/com/gabriel/os/resources/ClienteResource.java
@@ -1,12 +1,22 @@
 package com.gabriel.os.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabriel.os.domain.Cliente;
 import com.gabriel.os.dtos.ClienteDTO;
 import com.gabriel.os.services.ClienteService;
 
@@ -23,6 +33,26 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll(){
+		List<ClienteDTO> listCliente = clienteService.findAll().stream()
+				.map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listCliente);	
+	}
+	
+	/*
+	 * Cria um Cliente
+	 */
+	@PostMapping
+	public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteDTO objDTO){
+		Cliente newCliente = clienteService.create(objDTO);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newCliente.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
 	
 	
 
--- src/main/java/com/gabriel/os/services/ClienteService.java
+++ src/main/java/com/gabriel/os/services/ClienteService.java
@@ -1,16 +1,24 @@
 package com.gabriel.os.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.os.domain.Cliente;
+import com.gabriel.os.domain.Pessoa;
+import com.gabriel.os.dtos.ClienteDTO;
 import com.gabriel.os.repositories.ClienteRepository;
+import com.gabriel.os.repositories.PessoaRepository;
+import com.gabriel.os.resources.exceptions.DataIntegratyViolationException;
 import com.gabriel.os.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
@@ -20,5 +28,26 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + " Tipo: " + Cliente.class.getName()));
 	}
+	
+	public List<Cliente> findAll(){
+		return clienteRepository.findAll();
+	}
+	
+	public Cliente create(ClienteDTO objDTO) {
+		if(findByCPF(objDTO) != null) {
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+		}
+		
+		return clienteRepository.save(new Cliente(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+	}
+	
+	public Pessoa findByCPF(ClienteDTO objDTO) {
+		Pessoa pessoa = pessoaRepository.findByCPF(objDTO.getCpf());
+		if(pessoa != null) {
+			return pessoa;
+		}
+		return null;
+	}
+	
 
 }
